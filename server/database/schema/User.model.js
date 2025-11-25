23579553f5e1ae78731907365e017eda237c47f2
deveshanim3 <deveshsingh6986@gmail.com>
2025-11-25 21:54:22 +0530

    updated schema

--- server/database/schema/User.model.js
+++ server/database/schema/User.model.js
@@ -8,6 +8,10 @@ const userSchema= new mongoose.Schema({
         unique:true,
         trim:true,
         lowercase:true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 
+            'Please fill a valid email address'
+        ]
     },
     password:{
         type: String,
