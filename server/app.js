10b4df336ae2a261521ee09c6f69fd084a713a7b
deveshanim3 <deveshsingh6986@gmail.com>
2025-12-04 20:57:36 +0530

    testing deployment

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ app.use(express.json())
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5173",process.env.BASE_URL],
+  origin: ["http://localhost:5173",               
+    "https://stocktrackerxoox.netlify.app"],
   credentials: true,
 }));
 
